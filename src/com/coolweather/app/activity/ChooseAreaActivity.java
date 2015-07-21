63f4f6499816325e8206d34d234b846b16b612c4
ceociocto <ceociocto@qq.com>
2015-07-21 13:34:28 +0800

    加入显示天气信息的功能

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -49,10 +52,21 @@ public class ChooseAreaActivity extends Activity {
 	private City selectedCity;
 //	挡墙选中的级别
 	private int currentLevel;
+//	是否从WeatherActivity中跳转过来
+	private boolean isFromWeatherActivity;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//		已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if(prefs.getBoolean("city_selected", false) && !isFromWeatherActivity){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_choose_area);
 		listView = (ListView)findViewById(R.id.list_view);
@@ -69,6 +83,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if (currentLevel == LEVEL_CITY){
 					selectedCity = cityList.get(index);
 					queryCountries();
+				}else if(currentLevel == LEVEL_COUNTRY){
+					String countryCode = countryList.get(index).getCountryCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("country_code",countryCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
@@ -203,6 +223,10 @@ public class ChooseAreaActivity extends Activity {
 		}else if(currentLevel == LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if(isFromWeatherActivity){
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
